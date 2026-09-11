be87559063a870e06e0e2b211a66bcd3a5d54881
Sebastián <sebasunivercal@hotmail.com>
2026-09-10 22:51:44 -0500

    feat agrega el telefono al modelo estudiante

---
+++ Migrations/20260911033657_AddTelefonoEstudiante.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProgramacionV.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTelefonoEstudiante : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Telefono",
+                table: "Estudiantes",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Estudiantes",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Telefono",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Estudiantes",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Telefono",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Estudiantes",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Telefono",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Estudiantes",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Telefono",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Estudiantes",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Telefono",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Telefono",
+                table: "Estudiantes");
+        }
+    }
+}
--- Models/Estudiante.cs
+++ Models/Estudiante.cs
@@ -9,6 +9,7 @@ public class Estudiante
     public string Nombre { get; set; } = string.Empty;
 
     public string Correo { get; set; } = string.Empty;
+    public string? Telefono { get; set; }
 
     public int ProgramaAcademicoId { get; set; }
 
